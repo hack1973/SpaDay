7261421a098210075df8b5884a83a40094e6b6a8
Michael Hacker <hack1973@swbell.net>
2020-11-17 15:23:34 -0600

    completed CH14 studio Spa User Signup

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (verify == newUser.Password)
+            {
+                ViewBag.username = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                ViewBag.error = "Passwords do not match! Try again!";
+
+                return View("Add");
+            }
+        }
+    }
+}
